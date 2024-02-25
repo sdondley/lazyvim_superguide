1e68367646d471cb913d427653f009bbdfe1398f
Steve Dondley <s@dondley.com>
2024-02-25 18:07:42 -0500

    add custom settings

---
+++ nvim/custom/my_settings.lua
@@ -0,0 +1,11 @@
+vim.g.mapleader = ";"
+vim.api.nvim_set_keymap("n", "<leader>w", ":w<cr>", { noremap = true } )
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+
+
+
+
+
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,14 +1,7 @@
 -- lazy_init --
 
--- Below is the lua code for setting up the lazy.nvim package manager plugin.
--- This code is taken straight from the GitHub README file for lazy.vim:
--- https://github.com/folke/lazy.nvim
-
--- Assign 'lazypath' to the directory for downloading the lazy.nvim module
--- vim.fn.stdpath("data") is "~/.local/share/nvim" by default
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
--- if lazypath does not already exist, git clone it into lazypath
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
     "git",
@@ -20,34 +13,40 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 
--- Now add the lazypath directory to the front of the 'runtime path' option.
--- This ensures neovim will look in this directory first for plugins and other files at runtime.
--- See ":h rtp" in neovim for more details.
 vim.opt.rtp:prepend(lazypath)
 
 -- end lazy_init --
 
+-- custom --
+
+-- Now we'll load in a simple lua file containing some custom settings for the neovim editor.
+-- This is just the standard way of loading a file in Lua.
+-- Note the "." is a substitute for the "/" in the path.
+require("custom.my_settings");
+
+-- end custom --
+
 -- setup --
 
 require("lazy").setup(
-	{
-		-- theme --
-		-- Add a theme to make things a little prettier
-		-- These setting ensure the theme will be loaded early in the startup process
-		-- Settings are from the lazy.nvim README file with additional "opts" settings
-		{
-			"folke/tokyonight.nvim",
-			lazy = false,
-			priority = 1000,
-			init = function()
-				vim.cmd([[colorscheme tokyonight]])
-			end,
-			opts = { style = "moon", transparent = true }
-		}
-		-- end theme --
-	}, -- the plugin spec goes here
-
-	{}  -- our lazy.nvim options go here
+    {
+        -- theme --
+        -- Add a theme to make things a little prettier
+        -- These setting ensure the theme will be loaded early in the startup process
+        -- Settings are from the lazy.nvim README file with additional "opts" settings
+        {
+            "folke/tokyonight.nvim",
+            lazy = false,
+            priority = 1000,
+            init = function()
+                vim.cmd([[colorscheme tokyonight]])
+            end,
+            opts = { style = "moon", transparent = true }
+        },
+        -- end theme --
+    }, 
+    {}  -- our lazy.nvim options will go here
 )
+
 
 -- end setup --
