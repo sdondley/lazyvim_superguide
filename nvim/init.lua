56fc5be0e2923ed83d470412f73c63f912ed6194
Steve Dondley <s@dondley.com>
2024-02-25 14:15:47 -0500

    add additional explanatory comments to init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,10 @@
 -- lazy_init --
 
--- assign 'lazypath' to the directory for downloading the lazy.nvim module
+-- Below is the lua code for setting up the lazy.nvim package manager plugin.
+-- This code is taken straight from the GitHub README file for lazy.vim:
+-- https://github.com/folke/lazy.nvim
+
+-- Assign 'lazypath' to the directory for downloading the lazy.nvim module
 -- vim.fn.stdpath("data") is "~/.local/share/nvim" by default
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
@@ -16,8 +20,9 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 
--- add the lazypath directory to the front of the 'runtime path' option
--- this ensures neovim will look in this directory first for plugins and other files at runtime
+-- Now add the lazypath directory to the front of the 'runtime path' option.
+-- This ensures neovim will look in this directory first for plugins and other files at runtime.
+-- See ":h rtp" in neovim for more details.
 vim.opt.rtp:prepend(lazypath)
 
 -- end lazy_init --
